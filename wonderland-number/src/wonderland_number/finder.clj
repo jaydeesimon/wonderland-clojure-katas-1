dcdf4e3c0c67a61851a3078ef33fa4fe065d1ac7
Jeffrey Simon <jdsimon@gmail.com>
2017-02-05 18:11:26 -0500

    wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,13 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [n m]
+  (let [f (comp set str)]
+    (= (f n) (f m))))
+
+(defn wonderland-number? [n]
+  (->> (map (partial * n) [2 3 4 5 6])
+       (every? (partial same-digits? n))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range 100000 (inc 999999))
+       (some #(and (wonderland-number? %) %))))
